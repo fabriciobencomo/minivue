f31841b4f3031b8f5425b1781055d2130a63e98c
Fabricio Bencomo <Fabriciobencomo.viloria@gmail.com>
2022-08-13 07:03:54 -0400

    effect trigger and track created

--- main.js
+++ main.js
@@ -1,27 +1,63 @@
 class Reactivity{
+    // Dependencies
+    deps = new Map();
     constructor(options){
         this.src = options.data(); 
-        
-        //to 
-        
+        const self = this;
+        // Destino
         this.$data = new Proxy(this.src, {
             get(target, name){
-                return target[name]
+                if(Reflect.has(target, name)){
+                    self.track(target, name); 
+                    return Reflect.get(target, name);                    
+                }
+                console.warn('La propiedad', name, 'no existe');
+                return "";
+            },
+            set(target, name, value){
+                Reflect.set(target, name, value);
+                self.trigger(name);
             }
         });
+    }
+
+    track(target, name){
+        if(!this.deps.has(name)){
+            const effect = () => {
+                document.querySelectorAll(`*[p-text=${name}]`).forEach(el => {
+                     this.pText(el, target, name);
+                }); 
+            }
+            this.deps.set(name, effect)
+        }
+    }
+
+    trigger(name){
+        const effect = this.deps.get(name);
+        effect();
     }
 
     mount(){
         document.querySelectorAll("*[p-text]").forEach(el => {
             this.pText(el, this.$data, el.getAttribute("p-text"));  
         });
+        document.querySelectorAll("*[p-model]").forEach(el => {
+            const name = el.getAttribute("p-model");
+            this.pModel(el, this.$data, name);
+            el.addEventListener('input', () => {
+                Reflect.set(this.$data, name, el.value)
+            })
+        })
     }
     
     pText(el, target, name){
-         el.innerText = target[name];
+         el.innerText = Reflect.get(target, name);
     }
 
-    pModel(){}
+    pModel(el, target, name){
+        el.value = Reflect.get(target, name);
+    }
+    
 }
 
 
